e3f0122ddab032efccb5d324978b4e9ad1aa552e
Shailendra Jain <shailendrajain28@gmail.com>
2024-01-25 06:36:05 +0000

    updated code

--- pos_of_right_most_diff_bit.py
+++ pos_of_right_most_diff_bit.py
@@ -2,24 +2,17 @@ import sys
 
 
 def posOfRightMostDiffBit(m,n):
-    count=1
-    while True:
-        print(f"bin of m : {bin(m)}")
-        print(f"bin of n : {bin(n)}")
-        k = m & (~(m-1))
-        l = n & (~(n-1))
-        print(f"bin of k : {bin(k)}")
-        print(f"bin of l : {bin(l)}")
-        if k & l != 0:
-            count+=1
-            m>>=1
-            n>>=1
-        else:
-            print("returning count")
-            print(f"bin of k : {bin(k)}")
-            print(f"bin of l : {bin(l)}")
-            return count 
+    xor_result = m ^ n 
+
+    if xor_result == 0 : 
+        return -1 
     
+    count=1
+    while xor_result & 1 != 1:
+        count+=1
+        xor_result>>=1
+    
+    return count
 
 m = int(sys.argv[1])
 n = int(sys.argv[2])
